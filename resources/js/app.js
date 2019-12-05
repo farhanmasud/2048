c61008bb8b8613062d70be2e6baf72e7cb88e887
Farhan Masud <farhan.aneek@gmail.com>
2019-12-06 00:37:27 +0600

    JS edit 06.12.19

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,93 @@
 console.log("Hola!");
+
+// Data Controller
+
+var dataController = (function () {
+    // Code goes here
+
+    // gird is the variable where all the current values in the grid is stored
+
+    // var grid = [
+    //     [0, 0, 0, 0],
+    //     [0, 0, 0, 0],
+    //     [0, 0, 0, 0],
+    //     [0, 0, 0, 0]
+    // ];
+
+    var grid = [];
+    var score = 0;
+
+
+    return {
+        init_grid: function (height, width) {
+            // var grid = [];
+            var grid_row;
+
+            for (var i = 0; i < height; i++) {
+                grid_row = new Array(width).fill(0);
+                grid.push(grid_row);
+            }
+        },
+
+        // testing function
+        show_grid: function () {
+            console.log(grid);
+        },
+
+        calculate_score: function () {
+
+        },
+
+        init_score: function () {
+            score = 0;
+        }
+
+
+    };
+
+
+})();
+
+// UI Controller
+
+var uiController = (function () {
+    // Code goes here
+    var DOMStrings = {
+        tile: '#tile-',
+        score_box: ".score"
+    };
+
+
+
+    return {
+        DOMStrings: DOMStrings
+    }
+})();
+
+// Main Controller
+
+var controller = (function (dataCtrl, uiCtrl) {
+    // Code goes here
+
+    return {
+        init: function () {
+            console.log("Hi, I am init function");
+
+            // initialize the grid with height and row
+            dataCtrl.init_grid(4, 4);
+
+            // show the grid for testing
+            dataCtrl.show_grid();
+
+            // set the score to 0
+            dataCtrl.init_score();
+
+            // update the score on the UI
+
+
+        }
+    }
+})(dataController, uiController);
+
+
+controller.init();
